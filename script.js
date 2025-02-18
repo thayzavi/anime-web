88adec20db8a69291bf92b530642324bb2d52f8f
Thay.min <134392865+thayzavi@users.noreply.github.com>
2025-02-18 12:46:12 +0000

    js

--- script.js
+++ script.js
@@ -41,3 +41,52 @@ document.addEventListener("click", function(event){
     if (!menu.contains(event.target) && !icon.contains(event.target))menu.style.display = "none";
 });
 
+// logica login
+ // função para validar o form
+ function validarFormulario(){
+    var Usuario = document.getElementById("Usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if (Usuario === ""){
+        alert("Por favor preecha o campo usuário.");
+        return false;
+    }
+    if (senha === ""){
+        alert("Confirme a senha.");
+        return false;
+    }
+    redirecionarhome();
+}
+// função para ir para outra página
+function redirecionarhome (){
+window.location.href = "index.html"
+}
+// logica cadastro
+// função para validar o form
+function validarFormulario(){
+    var Usuario = document.getElementById("Usuario").value;
+    var contato  = document.getElementById("senha").value;
+    var senha = document.getElementById("senha").value;
+    var confirmasenha = document.getElementById("confirmasenha").value;
+    if (Usuario === ""){
+        alert("Por favor preecha o Nome.");
+        return false;
+    }
+    if (contato === ""){
+        alert("Preecha o campo email.");
+        return false;
+    }
+    if (senha === ""){
+        alert("Por favor preecha o campo senhar.");
+        return false;
+    }
+    if (confirmasenha === ""){
+        alert("Confirme a senha.");
+        return false;
+    }
+    redirecionarhome();
+}
+// função para ir para outra página
+function redirecionarhome (){
+window.location.href = "index.html"
+}
